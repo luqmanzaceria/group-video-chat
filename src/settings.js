3a012afe8a93ad9911f5411730b980d21087f67e
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-12-21 16:39:56 -0800

    Agora auth and React dependency installation

---
+++ src/settings.js
@@ -0,0 +1,2 @@
+const appId = "ad43225cf464417e887ce8f4f6e5e883"
+const token = "007eJxTYPjf8Z1vnUmz7clKzfDDbVqGcy58cJPbfPfC9auvbm1fnn9agSExxcTYyMg0Oc3EzMTE0DzVwsI8OdUizSTNLNUUyDEOut6S2hDIyHBQT5OJkQECQXwWhtzEzDwGBgAteiIm"
