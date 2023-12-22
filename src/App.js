352f1715c73a31347bf956078cd19849ddf83bc3
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-12-21 21:42:17 -0800

    Fixed video display errors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { useState } from "react";
-import  { Button } from "@material-ui/core";
-import VideoCall from "./VideoCall"
+import { Button } from "@material-ui/core";
+import VideoCall from "./VideoCall";
 
 function App() {
   const [inCall, setInCall] = useState(false);
 
   return (
-    <div className="App">
-      <Button variant="contained" color="primary" onClick={ () => setInCall(true)}>Join Call</Button>
-      { inCall ? <VideoCall setInCall={setInCall} /> : "Waiting to join call!"}
+    <div className="App" style={{ height: "100%" }}>
+      {inCall ? (
+        <VideoCall setInCall={setInCall} />
+      ) : (
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setInCall(true)}
+        >
+          Join Call
+        </Button>
+      )}
     </div>
   );
 }
--- src/VideoCall.js
+++ src/VideoCall.js
@@ -1,74 +1,81 @@
 import { useState, useEffect } from "react";
-import { config, useClient, useMicrophoneAndCameraTracks, channelName} from "./settings.js"
+import {
+  config,
+  useClient,
+  useMicrophoneAndCameraTracks,
+  channelName,
+} from "./settings.js";
 import { Grid } from "@material-ui/core";
 import Video from "./Video";
-import Controls from "./Controls"
+import Controls from "./Controls";
 
 export default function VideoCall(props) {
-    const { setInCall } = props;
-    const [users, setUsers] = useState([]);
-    const [start, setStart] = useState(false);
-    const client = useClient();
-    const { ready, tracks } = useMicrophoneAndCameraTracks();
+  const { setInCall } = props;
+  const [users, setUsers] = useState([]);
+  const [start, setStart] = useState(false);
+  const client = useClient();
+  const { ready, tracks } = useMicrophoneAndCameraTracks();
 
-    useEffect(() => {
-        let init = async (name) => {
-            client.on("user-published", async (user, mediaType) => {
-                await client.subscribe(user, mediaType);
-                if (mediaType === "video") {
-                    setUsers((prevUsers) => {
-                        return [...prevUsers, user];
-                    })
-                }
-                if (mediaType === "audio") {
-                    user.audioTrack.play();
-                }
-            });
-
-            client.on("user-published", (user, mediaType) => {
-                if (mediaType === "audio") {
-                    if (user.audioTrack) user.audioTrack.stop();
-                }
-                if (mediaType === "video") {
-                    setUsers((prevUsers) => {
-                        return prevUsers.filter((User) => User.uid !== user.uid);
-                    });
-                }
-            });
-
-            client.on("user-left", (user => {
-                setUsers((prevUsers) => {
-                    return prevUsers.filter((User) => User.uid !== user.uid);
-                });
-            }));
-
-            try {
-                await client.join(config.appId, name, config.token, null)
-            } catch (error) {
-                console.log("error")
-            }
-
-            if (tracks) await client.publish([tracks[0], tracks[1]]);
-            setStart(true);
-        };
-
-        if (ready && tracks) {
-            try {
-                init(channelName);
-            } catch (error) {
-                console.log(error);
-            }
+  useEffect(() => {
+    let init = async (name) => {
+      client.on("user-published", async (user, mediaType) => {
+        await client.subscribe(user, mediaType);
+        if (mediaType === "video") {
+          setUsers((prevUsers) => {
+            return [...prevUsers, user];
+          });
         }
-    }, [channelName, client, ready, tracks])
+        if (mediaType === "audio") {
+          user.audioTrack.play();
+        }
+      });
 
-    return (<Grid container direction="column" style={{ height: "100%" }}>
-        <Grid item style={{height: "5%"}}>
-            { ready && tracks && (
-            <Controls tracks={tracks} setStart={start} setInCall={setInCall} />
-            )}
-        </Grid>
-        <Grid item style={{height: "95%"}}>
-            { start && tracks && (<Video tracks={tracks} users={users} />)}
-        </Grid>
-    </Grid>)
+      client.on("user-unpublished", (user, mediaType) => {
+        if (mediaType === "audio") {
+          if (user.audioTrack) user.audioTrack.stop();
+        }
+        if (mediaType === "video") {
+          setUsers((prevUsers) => {
+            return prevUsers.filter((User) => User.uid !== user.uid);
+          });
+        }
+      });
+
+      client.on("user-left", (user) => {
+        setUsers((prevUsers) => {
+          return prevUsers.filter((User) => User.uid !== user.uid);
+        });
+      });
+
+      try {
+        await client.join(config.appId, name, config.token, null);
+      } catch (error) {
+        console.log("error");
+      }
+
+      if (tracks) await client.publish([tracks[0], tracks[1]]);
+      setStart(true);
+    };
+
+    if (ready && tracks) {
+      try {
+        init(channelName);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }, [channelName, client, ready, tracks]);
+
+  return (
+    <Grid container direction="column" style={{ height: "100%" }}>
+      <Grid item style={{ height: "5%" }}>
+        {ready && tracks && (
+          <Controls tracks={tracks} setStart={setStart} setInCall={setInCall} />
+        )}
+      </Grid>
+      <Grid item style={{ height: "95%" }}>
+        {start && tracks && <Video tracks={tracks} users={users} />}
+      </Grid>
+    </Grid>
+  );
 }
